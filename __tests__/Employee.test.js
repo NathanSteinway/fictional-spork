4b9ec0b647a2b1a6e62ab801ff219ebbadbbde44
unknown <steinway.nathan@gmail.com>
2022-04-09 19:14:33 -0500

    ID, name, email, and object creation for Employee pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,37 @@
 const Employee = require('../lib/Employee');
 
-test('create employee object', () => {
+test('create Employee object', () => {
+
     const employee = new Employee()
 
     expect(typeof(employee)).toBe('object');
+
 });
+
+test('add name to Employee object', () => {
+
+    const newName = "Nathan";
+
+    const employeeObject = new Employee(newName);
+
+    expect(employeeObject.getName()).toBe(newName);
+
+});
+
+test("add an ID to the Employee object", () => {
+
+    const id = 5;
+
+    const employeeInstance = new Employee("Nathan", id);
+
+    expect(employeeInstance.id).toBe(id);
+})
+
+test("add an email to the Employee object", () => {
+
+    const email = "steinway.nathan@gmail.com";
+
+    const employeeInstance = new Employee("Nathan", 5, email);
+
+    expect(employeeInstance.email).toBe(email);
+})
