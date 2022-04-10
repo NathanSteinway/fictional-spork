62c44bb250619ec4760bf5965342d6d896f29de5
unknown <steinway.nathan@gmail.com>
2022-04-09 19:48:15 -0500

    Employee.js passes all tests!

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -35,3 +35,45 @@ test("add an email to the Employee object", () => {
 
     expect(employeeInstance.email).toBe(email);
 })
+
+// Test functions
+
+test("Tests getName();", () => {
+
+    const testName = "Nathan";
+
+    const employeeInstance = new Employee(testName);
+
+    expect(employeeInstance.getName()).toBe(testName);
+
+})
+
+test("Tests getID();", () => {
+
+    const testID = 5;
+
+    const employeeInstance = new Employee("Nathan", testID);
+
+    expect(employeeInstance.getId()).toBe(testID);
+
+})
+
+test("Tests getEmail();", () => {
+
+    const testEmail = "steinway.nathan@gmail.com";
+
+    const employeeInstance = new Employee("Nathan", 5, testEmail);
+
+    expect(employeeInstance.getEmail()).toBe(testEmail);
+
+})
+
+test("Testing role.", () => {
+
+    const returnValue = "Employee";
+
+    const employeeInstance = new Employee("Nathan", 5, "steinway.nathan@gmail.com");
+
+    expect(employeeInstance.getRole()).toBe(returnValue);
+    
+})
