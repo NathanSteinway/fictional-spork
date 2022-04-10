855650ca97ff6d24b80beeef761fad3d32542b7e
unknown <steinway.nathan@gmail.com>
2022-04-09 22:37:40 -0500

    SO many bug fixes, finishing touches

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,7 +3,7 @@ const generateHTML = function (employees) {
     const generateManager = function (manager) {
         return `
         <div class="card employee-card">
-            <div class="card-header bg-primary text-white">
+            <div class="card-header card-header-color text-dark">
                 <h2 class="card-title">${manager.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
             </div>
@@ -12,7 +12,7 @@ const generateHTML = function (employees) {
                     <li class="list-group-item">ID: ${manager.getId()}</li>
                     <li class="list-group-item">Email: 
                         <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                    <li class="list-group-item">Office number: ${manager.officeNumber}</li>
                 </ul>
             </div>
         </div>
@@ -22,7 +22,7 @@ const generateHTML = function (employees) {
     const generateEngineer = function (engineer) {
         return `
         <div class="card employee-card">
-            <div class="card-header bg-primary text-white">
+            <div class="card-header card-header-color text-dark">
                 <h2 class="card-title">${engineer.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
             </div>
@@ -32,7 +32,7 @@ const generateHTML = function (employees) {
                     <li class="list-group-item">Email: 
                         <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
                     <li class="list-group-item">GitHub: 
-                        <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                        <a href="https://github.com/${engineer.getGitHub()}" target="_blank" rel="noopener noreferrer">${engineer.getGitHub()}</a></li>
                 </ul>
             </div>
         </div>
@@ -42,7 +42,7 @@ const generateHTML = function (employees) {
     const generateIntern = function (intern) {
         return `
         <div class="card employee-card">
-            <div class="card-header bg-primary text-white">
+            <div class="card-header card-header-color text-dark">
                 <h2 class="card-title">${intern.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
             </div>
@@ -83,32 +83,32 @@ module.exports = employees => {
     return `
     <!DOCTYPE html>
     <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link rel="stylesheet" href="./style.css">
-        </head>
-
-        <body>
-            <div class="header-contianer">
-                <div class="row">
-                    <div class="header">
-                        <h1 class="my-team">My Team</h1>
-                    </div>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 hero mb-3 team-heading">
+                    <h1 class="text-center hero-text-color">My Team</h1>
                 </div>
             </div>
-            <div class="main-contianer">
-                <div class="row">
-                    <div class="content-area">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="row team-area col-12 d-flex justify-content-center">
                     ${generateHTML(employees)}
-                    </div> 
                 </div>
             </div>
-        </body>
-
-    </html>  
-    `
+        </div>
+    </body>
+    </html>
+        `;
 }
